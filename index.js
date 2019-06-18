0e5950ad3e85c438583822b761b28e81ed5fa83d
Rob Ballantyne <raballantyne@gmail.com>
2019-06-18 19:17:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {key:'value'}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var updateObject = `{
+  var updateObject = {
   //return Object.assign({},object,{[key]:value})
 }
 
